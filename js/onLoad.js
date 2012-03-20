965b35a372c504d5a552d7f6c2be2695f7d73d43
Maxime Gaudin <mgaudin@nation.(none)>
2012-03-20 13:06:51 +0100

    Ajout de la gallerie de compétences (en developpement) et réalisation du rubban CSS

--- js/onLoad.js
+++ js/onLoad.js
@@ -1,7 +1,17 @@
 $(document).ready( function() {
   $(".toFade:hidden:first").fadeIn("slow", function () { 
     $(".toFade:hidden:first").fadeIn("slow", function () {
+    $(".toFade:hidden:first").fadeIn("slow", function () {
+    $(".toFade:hidden:first").fadeIn("slow", function () {
+    $(".toFade:hidden:first").fadeIn("slow", function () {
+    $(".toFade:hidden:first").fadeIn("slow", function () {
+    $(".toFade:hidden:first").fadeIn("slow", function () {
       $(".toFade:hidden:first").fadeIn("slow");
+    });
+    });
+    });
+    });
+    });
     });
   });
 });
